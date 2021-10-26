b87b7ba360970035cadd41d2386d603950613cda
InspireGeo <aytacaraz@gmail.com>
2021-10-26 19:14:48 +0200

    bu aksam

--- article/views.py
+++ article/views.py
@@ -4,6 +4,7 @@ import article
 from .forms import ArticleForm
 from django.contrib import messages
 from .models import Article
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -12,13 +13,14 @@ def index(request):
 def about(request):
     return render(request,"about.html")
 
+@login_required(login_url = "user:login")
 def dashboard(request):
     articles = Article.objects.filter(author = request.user)
     context = {
         "articles":articles
     }
     return render(request, "dashboard.html",context)
-
+@login_required(login_url = "user:login")
 def addarticle(request):
     form = ArticleForm(request.POST or None,request.FILES or None)
 
@@ -32,13 +34,13 @@ def addarticle(request):
     
     return render(request, "addarticle.html",{"form":form})
  
-
+@login_required(login_url = "user:login")
 def detail(request,id):
     #article = Article.objects.filter(id=id).first
     
     article = get_object_or_404(Article,id = id)
     return render(request,"detail.html",{"article":article})
-
+@login_required(login_url = "user:login")
 def updateArticle(request,id):
     article = get_object_or_404(Article,id = id)
     form = ArticleForm(request.POST or None,request.FILES or None, instance = article)
@@ -51,7 +53,7 @@ def updateArticle(request,id):
         messages.success(request,"Makale başarıyla Güncellendi")
         return redirect("article:dashboard")
     return render(request,"update.html",{"form":form})
-
+@login_required(login_url = "user:login")
 def deleteArticle(request,id):
     article = get_object_or_404(Article,id = id)
    
